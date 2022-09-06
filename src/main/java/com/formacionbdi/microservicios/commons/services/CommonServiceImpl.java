4f228294c29c999feb9446d769c60297d1f94f8a
Ruben-developer <rcontreras.dev@protonmail.com>
2022-09-06 13:54:42 -0300

    Se modifica la carpeta del proyecto

--- src/main/java/com/formacionbdi/microservicios/commons/services/CommonServiceImpl.java
+++ src/main/java/com/formacionbdi/microservicios/commons/services/CommonServiceImpl.java
@@ -3,6 +3,8 @@ package com.formacionbdi.microservicios.commons.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.transaction.annotation.Transactional;
 
